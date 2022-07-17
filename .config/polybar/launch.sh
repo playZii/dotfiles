2d3c681ac0731d87ebd5a46e2246ea9db2001138
playZii <playzii.kontakt@gmail.com>
2022-07-17 18:33:54 +0200

    Created auto detection script for CPU temperature

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
 # If all your bars have ipc enabled, you can also use 
 polybar-msg cmd quit
 
@@ -14,6 +15,5 @@ else
 fi
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-#polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
